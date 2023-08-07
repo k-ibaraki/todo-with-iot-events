f8ec5724c31590efd67d693f068598271719b9f8
k-ibaraki <ibaraki@ncdc.co.jp>
2023-08-07 16:16:57 +0900

    detector model 追加

--- lib/iot-events-stack.ts
+++ lib/iot-events-stack.ts
@@ -3,18 +3,99 @@ import { Construct } from 'constructs';
 import * as iotevents from 'aws-cdk-lib/aws-iotevents';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
+// dotenv
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
+
+///* 諸々の定義 * ///
+
+// input
+const inputName = 'sample_task_input';
+const inputAtts = {
+  id: 'id',
+  title: 'title',
+  done: 'done',
+};
+const inputAttribute: cdk.aws_iotevents.CfnInput.AttributeProperty[] = Object.values(inputAtts).map(
+  (att) => { return { "jsonPath": att } }
+);
+
+// detector model
+const detectorModelRoleName = 'sample_task_detector_model_role';
+const detectorModelRoleArn = `arn:aws:iam::${AWS_ACCOUNT_ID}:role/${detectorModelRoleName}`;
+const detectorModelName = 'sample_task_detector_model';
+const detectorModelDefinition = {
+  states: [
+    {
+      stateName: "todo",
+      onInput: {
+        events: [
+          {
+            eventName: "setTitle",
+            condition: `! isUndefined( $input.${inputName}.${inputAtts.title} )`,
+            actions: [
+              {
+                setVariable: {
+                  variableName: inputAtts.title,
+                  value: `$input.${inputName}.${inputAtts.title}`,
+                },
+              },
+            ],
+          },
+        ],
+        transitionEvents: [
+          {
+            eventName: "do",
+            condition: `$input.${inputName}.${inputAtts.done} == true`,
+            actions: [],
+            nextState: "done",
+          },
+        ],
+      },
+      onEnter: {
+        events: [
+          {
+            eventName: "setDefaultTitle",
+            condition: `isUndefined( $input.${inputName}.${inputAtts.title} )`,
+            actions: [
+              {
+                setVariable: {
+                  variableName: inputAtts.title,
+                  value: `'task' + convert (String, $input.${inputName}.${inputAtts.id})`,
+                }
+              },
+            ],
+          },
+        ],
+      },
+      onExit: {
+        events: [],
+      }
+    },
+    {
+      stateName: "done",
+      onInput: {
+        events: [],
+        transitionEvents: [],
+      },
+      onEnter: {
+        events: [],
+      },
+      onExit: {
+        events: [],
+      }
+    }
+  ],
+  initialStateName: "todo",
+};
+
+
 
 export class IotEventsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // 定数定義
-    const inputName = 'sample_task_input';
-    const inputAttribute = [
-      { jsonPath: 'sample_task.id' },
-      { jsonPath: 'sample_task.title' },
-      { jsonPath: 'sample_task.done' },
-    ] as cdk.aws_iotevents.CfnInput.AttributeProperty[];
 
     // AWS IoT Events Input
     const iotEventsInput = new iotevents.CfnInput(this, inputName, {
@@ -24,5 +105,16 @@ export class IotEventsStack extends cdk.Stack {
         attributes: inputAttribute
       },
     });
+
+    // AWS IoT Events Detector Model
+    const detectorModel = new iotevents.CfnDetectorModel(this, detectorModelName, {
+      detectorModelName: detectorModelName,
+      detectorModelDescription: 'sample detector model from cdk',
+      evaluationMethod: "SERIAL",
+      key: 'sample_task.id',
+      detectorModelDefinition: detectorModelDefinition,
+      roleArn: detectorModelRoleArn,
+    });
   }
 }
+
